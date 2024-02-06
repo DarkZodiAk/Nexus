bb1ef1eb73fa3e442afa73763e4a4c3771054825
DarkZodiAk <evgenyp2004@gmail.com>
2024-02-06 19:07:42 +0500

    Базовая верстка

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="col-10 col-lg-8 centered_page space_between">
+        <span id="logo">
+            <a href="/" >Nexus</a>&nbsp;&nbsp;
+        </span>
+            <div class="sign">
+                <a href="/">
+                    <span>Регистрация</span>
+                </a>
+                <a href="/">
+                    <span>Войти</span>
+                </a>
+            </div>
+        </div>
+</header>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nexus</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <?php require 'header.php'; ?>
+
+    <div class="flex_row min_height">
+        <div class="side col-lg-2"
+             style="background: linear-gradient(-90deg, #f9f9ff 5%, #415f91 100%)"></div>
+        <div class="col-10 col-lg-8 centered_page">
+
+            <h2>Section 1</h2>
+            <div class="block">
+                <div class="section">
+                    <a href="/">
+                        <h4>C++</h4>
+                    </a>
+                    <div class="section-desc">Understandable, have a nice day</div>
+                    <div class="section-desc">Сообщений: 118</div>
+                </div>
+                <div class="divider"></div>
+                <div class="section">
+                    <h4>Kotlin</h4>
+                    <div class="section-desc">Some Kotlin</div>
+                    <div class="section-desc">Сообщений: 72</div>
+                </div>
+            </div>
+
+            <h2>Section 2</h2>
+            <div class="block">
+                <div class="section">
+                    <a href="/">
+                        <h4>C++</h4>
+                    </a>
+                    <div class="section-desc">Understandable, have a nice day</div>
+                    <div class="section-desc">Сообщений: 118</div>
+                </div>
+                <div class="divider"></div>
+                <div class="section">
+                    <h4>Kotlin</h4>
+                    <div class="section-desc">Some Kotlin</div>
+                    <div class="section-desc">Сообщений: 72</div>
+                </div>
+            </div>
+
+        </div>
+        <div class="side col-lg-2"
+             style="background: linear-gradient(90deg, #f9f9ff 5%, #415f91 100%)"></div>
+    </div>
+
+    <footer>
+        <div>Автор работы: Павлусенко Евгений Максимович</div>
+        <div>Почта: pavlusenko_em@edu.surgu.ru</div>
+    </footer>
+</body>
+</html>
