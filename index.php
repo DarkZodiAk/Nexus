f64c96586d6e74197fa670577b66a5c2ca3fdae2
DarkZodiAk <evgenyp2004@gmail.com>
2024-02-06 19:25:33 +0500

    Небольшое увеличение адаптивности

--- index.php
+++ index.php
@@ -20,18 +20,18 @@
 
             <h2>Section 1</h2>
             <div class="block">
-                <div class="section">
-                    <a href="/">
+                <a href="/">
+                    <div class="section">
                         <h4>C++</h4>
-                    </a>
-                    <div class="section-desc">Understandable, have a nice day</div>
-                    <div class="section-desc">Сообщений: 118</div>
-                </div>
+                        <div class="section-desc">Understandable, have a nice day</div>
+                        <div class="section-messages">Сообщений: 118</div>
+                    </div>
+                </a>
                 <div class="divider"></div>
                 <div class="section">
                     <h4>Kotlin</h4>
                     <div class="section-desc">Some Kotlin</div>
-                    <div class="section-desc">Сообщений: 72</div>
+                    <div class="section-messages">Сообщений: 72</div>
                 </div>
             </div>
 
@@ -42,13 +42,13 @@
                         <h4>C++</h4>
                     </a>
                     <div class="section-desc">Understandable, have a nice day</div>
-                    <div class="section-desc">Сообщений: 118</div>
+                    <div class="section-messages">Сообщений: 118</div>
                 </div>
                 <div class="divider"></div>
                 <div class="section">
                     <h4>Kotlin</h4>
                     <div class="section-desc">Some Kotlin</div>
-                    <div class="section-desc">Сообщений: 72</div>
+                    <div class="section-messages">Сообщений: 72</div>
                 </div>
             </div>
 
